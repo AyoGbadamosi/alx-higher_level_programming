d9c84d3bd39af1343da6954c267cb97cc39bd2f3
AyoGbadamosi <gbadamosiayo16@gmail.com>
2022-06-02 09:15:30 -0700

    python

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,8 @@
+able File  7 lines (5 sloc)  157 Bytes
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the value of variable a from variable_load_5."""
+    from variable_load_5 import a
+
+    print(a)
