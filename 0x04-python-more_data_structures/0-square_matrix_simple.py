29501b1063c6f32356ca8fe05abded3dd556d8b1
AyoGbadamosi <gbadamosiayo16@gmail.com>
2022-06-08 09:24:16 -0700

    Python - More Data Structures: Set, Dictionary

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    """computes the square value of all integers of a matrix."""
+    return list(map(lambda submat: list(map(lambda e: e**2, submat)), matrix))
