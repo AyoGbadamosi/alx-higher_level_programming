24b06f384ddccbfa4dddfe36af9b53649beab130
AyoGbadamosi <gbadamosiayo16@gmail.com>
2022-07-08 06:28:33 -0700

    Say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -10,7 +10,7 @@ def say_my_name(first_name, last_name=""):
     Print Full Name of a person.
     """
     if type(first_name) != str:
-        raise TypeError('First_name must be a string')
+        raise TypeError('first_name must be a string')
     if type(last_name) != str:
-        raise TypeError('Last_name must be a string')
+        raise TypeError('last_name must be a string')
     print("My name is " + first_name + " " + last_name)
